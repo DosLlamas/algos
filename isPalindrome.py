1994b4cd5a91f3e2487405f229f21d675af94a8e
Nathan Foss <nathanfoss.dev@gmail.com>
2023-04-21 00:38:36 -0500

    Solution for isPalindrome

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,1 +1,22 @@
-## isPalindrome?
+# Check if a string is a palindrome, return "YES" or "NO"
+def isPalindrome(string):
+    start = 0
+    end = len(string) - 1
+    while start < end :
+        if string[start] != string[end] :
+            return "NO"
+        start += 1
+        end -= 1
+
+    return "YES"
+
+
+# test1 = "tacocat"
+# test2 = "level"
+# test3 = "not"
+
+inp = input("Enter a word or palindrome: ")
+
+print(isPalindrome(inp))
+# print(isPalindrome(test2))
+# print(isPalindrome(test3))
