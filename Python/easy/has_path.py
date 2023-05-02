0d583567ce3f921069744ba8a695c36e1936477d
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-02 20:07:17 +0300

    DSF practice

--- Python/easy/has_path.py
+++ Python/easy/has_path.py
@@ -3,7 +3,19 @@ Write a function, has_path that takes in an object representing the adjaceny lis
 of a directed acyclic graph and two nodes (src, dst). The function should return
 a boolean indicating whether or not there exists a direct path between the source
 and destination nodes.
+
+Apprach #1 DFS:
+Steps:
+1.
+if src == dst return True
+2.
+check if there is a path through neighbors to dst
+3.
+4.
+return false if no path found
+
 """
+
 graph = {
     'f': ['g', 'i'],
     'g': ['h'],
