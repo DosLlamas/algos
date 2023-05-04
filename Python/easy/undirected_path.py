df2cec5a44fbc733988033e71e787d6ceaa35407
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-04 03:31:00 +0300

    Convert edges to graph

---
+++ Python/easy/undirected_path.py
@@ -0,0 +1,22 @@
+"""
+Write a function, undirected_path, that takes in an array of edges 
+for an undirected graph and two nodes (nodeA, nodeB). The function
+should return a boolean indicating whether or not there exists
+a path between nodeA and nodeB.
+
+Apprach#1: 
+Steps:
+1.
+
+"""
+
+edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
+
+
+# print(undirected_path(edges, 'j', 'm'))
