3137b96c3fc77cfc13fb599f3de153ad9931381d
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-07 01:25:18 +0300

    Trying to remember Brian's interview

--- Python/easy/Brian_Interview.py
+++ Python/easy/Brian_Interview.py
@@ -1,2 +1,9 @@
-# record(12)    record(13)    record(5)     re
-# Brian's interview, maybe I can remember how it went later
+'''
+Brian's interview:
+Naybe I can remember how it went later
+
+record(12)    record(13)    record(5)     re
+
+Find how many times record() is called in the interval? 
+I'll look it up online lol
+'''
