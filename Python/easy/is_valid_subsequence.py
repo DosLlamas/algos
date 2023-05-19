d6bac49347c46cccc1d2f674f5fa434b0390b92a
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-19 12:42:14 -0500

    finish solution

---
+++ Python/easy/is_valid_subsequence.py
@@ -0,0 +1,29 @@
+'''
+Given two non-empty arrays of integers, write a function that determines
+whether the second array is a subsequence of the first one.
+
+A subsequence of an array is a set of numbers that aren't necessarily adjacent
+in the array but that are in the same order as they appear in the array. For
+instance, the numbers
+
+Sample Input
+  array = [5,1,22,25,6,-1,8,10]
+  sequence = [1,6,-1,10]
+
+Sample Output
+  true
+
+
+Approach O(n) time O(1) space
+'''
+
+def isValidSubsequence(array, sequence):
+    sequence_pointer = 0
+    for i in array:
+        if sequence_pointer == len(sequence):
+            return True
+        if sequence[sequence_pointer] == i:
+            sequence_pointer += 1
+    return sequence_pointer == len(sequence)
+            
+isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],  [1, 6, -1, 10])
