aa04704f95913d2e0598fa5fe3ae393da7ab74b5
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-19 16:02:51 -0500

    binary search

---
+++ Python/easy/binary_search.py
@@ -0,0 +1,16 @@
+'''
+Write a function that takes in a sorted array of integers as well as a target
+integer. The function should use the Binary Search algorithm to determine if
+the target integer is contained in the array and should return its index if it
+is, otherwise
+
+Sample Input
+arr = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+
+Sample Output
+3
+'''
+
+def binarySearch(array, target):
+    # Write your code here.
+    pass
