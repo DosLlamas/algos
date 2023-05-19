d3a8cbb90d01e00a9d80f085868a1fa1d631bf73
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-19 18:14:55 -0500

    finish binary search solution

--- Python/easy/binary_search.py
+++ Python/easy/binary_search.py
@@ -2,15 +2,31 @@
 Write a function that takes in a sorted array of integers as well as a target
 integer. The function should use the Binary Search algorithm to determine if
 the target integer is contained in the array and should return its index if it
-is, otherwise
+is, otherwise return -1
 
 Sample Input
 arr = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
 
 Sample Output
 3
+
+Approach
+O(log n) time
+O(1) space
 '''
 
 def binarySearch(array, target):
-    # Write your code here.
-    pass
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
+
